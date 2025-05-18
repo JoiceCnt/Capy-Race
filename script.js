445bed1140015be99f1743375af586ae15e18349
Joice Conte <joiceconte@gmail.com>
2025-05-18 11:17:14 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const gameContainer = document.getElementById("game");
+const capybaraElement = document.getElementById("capybara");
+const levelDisplay = document.getElementById("level");
+const scoreWaterBar = document.getElementById("bar-water");
+const scoreSunBar = document.getElementById("bar-sun");
+const scoreFertilizerBar = document.getElementById("bar-fertilizer");
+const scoreLadybugBar = document.getElementById("bar-ladybug");
+
+const fallingWater = document.getElementsByClassName("water");
+const fallingSun = document.getElementsByClassName("sun");
+const fallingFertilizer = document.getElementsByClassName("fertilizer");
+const fallingLadybug = document.getElementsByClassName("ladybug");
+
+const plant0Element = document.querySelector(".plant-0");
+const plant1Element = document.querySelector(".plant-1");
+const plant2Element = document.querySelector(".plant-2");
+const plant3Element = document.querySelector(".plant-3");
+const plant4Element = document.querySelector(".plant-4");
+const plant5Element = document.querySelector(".plant-5");
+const plant6Element = document.querySelector(".plant-6");
+const plant7Element = document.querySelector(".plant-7");
+
